cb3c84b179bf3872406ef272cfc72e757fc4b6a2
Vinicius <vinnievandro@hotmail.com>
2021-10-25 20:39:08 -0300

    Aula incial em java

---
+++ Aula2.js/Pt2.js
@@ -0,0 +1,16 @@
+
+let Idade = 27;
+let Peso = 83.5;
+let Altura = 1.70;
+let IndicedeMassa = "IMC";
+let Nome = "JOSE";
+let Plano = "Plano Ouro";
+
+//peso pela altura*2
+
+IMC = Peso / (Altura*2);
+
+
+console.log(Nome,Plano,Idade,Peso,Altura);
+console.log(IMC);
+console.log("Jose tem 27 anos e seu indiçe de massa corporal é de 24.5588");
---
+++ Aula2.js/ex2.js
@@ -0,0 +1,11 @@
+
+let nome = "Vinão"
+const idade = 24
+const usuarioAtivo = true
+ 
+console.log("Nome:")
+console.log(nome)
+console.log("Idade:")
+console.log(idade)
+console.log("ativão?")
+console.log(usuarioAtivo)
