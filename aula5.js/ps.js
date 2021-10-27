c4d03a8142685951f9c014cde6402c3a103e6a65
Vinicius <vinnievandro@hotmail.com>
2021-10-27 20:41:09 -0300

    mesa trab dia 27

---
+++ aula5.js/ps.js
@@ -0,0 +1,10 @@
+function StringChallenge(num) { 
+
+
+    // code goes here  
+    return (num,":"); 
+  
+  }
+     
+  // keep this function call here 
+  console.log(StringChallenge(readline()));
