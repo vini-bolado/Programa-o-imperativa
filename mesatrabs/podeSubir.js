4e56b73172ba398450bcb9f371ed80ed09a44f7d
Vinicius <vinnievandro@hotmail.com>
2021-11-08 20:42:37 -0300

    +

--- mesatrabs/podeSubir.js
+++ mesatrabs/podeSubir.js
@@ -4,18 +4,19 @@ function podeSubir (altura,acompanhante){
 
     if(altura <= 2.00 && altura >= 1.40){
         podeSubir = true
-        return true;
+        //return true;
         console.log("Acesso Autorizado");
 
     }
         else if(altura <=1.40 && altura >=1.20 && acompanhante){
             podeSubir = true
+            //return true
             console.log("Acesso Autorizado somente com acompanhante");
 
         }
         else if(altura <1.20){
             podeSubir = false
-            return false;
+            //return false;
             console.log("Acesso Negado");
         }
 
