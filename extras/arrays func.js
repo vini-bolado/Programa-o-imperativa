49d277510cf3cbf85fd2df027a0a849f88b88424
Vinicius <vinnievandro@hotmail.com>
2021-11-08 20:42:29 -0300

    aula 08/11

---
+++ extras/arrays func.js
@@ -0,0 +1,40 @@
+//join junta elementos de uma forma diferente
+
+let produtosCompra = ["carne","legumes","verduras"]
+
+// let separadoPorVirgula = produtosCompra.join("-")
+
+//     console.log(separadoPorVirgula);
+
+// pop elimina oultimo elemento do array
+
+    // produtosCompra.pop()
+
+    // console.log(produtosCompra);
+
+//push - adiciona elemento
+
+    // produtosCompra.push("racao");
+
+    // console.log(produtosCompra);
+
+//shift - elimina o primeiro
+
+    // produtosCompra.shift();
+
+    // console.log(produtosCompra);
+
+//unshifth adiciona elementos ao inicia da array
+
+    // produtosCompra.unshift("racao");
+        
+    // console.log(produtosCompra);
+
+
+
+
+
+
+
+
+    
---
+++ mesatrabs/podeSubir.js
@@ -0,0 +1,25 @@
+function podeSubir (altura,acompanhante){
+
+
+
+    if(altura <= 2.00 && altura >= 1.40){
+        podeSubir = true
+        return true;
+        console.log("Acesso Autorizado");
+
+    }
+        else if(altura <=1.40 && altura >=1.20 && acompanhante){
+            podeSubir = true
+            console.log("Acesso Autorizado somente com acompanhante");
+
+        }
+        else if(altura <1.20){
+            podeSubir = false
+            return false;
+            console.log("Acesso Negado");
+        }
+
+    
+}
+    console.log(podeSubir(1.18));
+    
